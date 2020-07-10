3a73743d90f27c5923fae072792c578baa752512
rishabharya <rishabharya2799@gmail.com>
2020-07-10 17:39:28 +0530

    new changes

---
+++ Searching/Missing number in array.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int MissingNumber(vector<int>& array, int n);
+
+// Position this line where user code will be pasted.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+
+        cout << MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+// User function template for C++
+
+int MissingNumber(vector<int>& array, int n) {
+    // Your code goes here
+    int total = (n + 1) * (n) / 2;
+    for(int i = 0 ; i < n - 1; i++)
+        total -= array[i];
+    return total ;
+}
